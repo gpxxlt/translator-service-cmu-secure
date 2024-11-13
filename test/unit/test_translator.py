c19be3e8259c288322bf45d28679a044233caebc
Yuepeng Gu <guyuepeng@air-model-g.wifi.local.cmu.edu>
2024-11-13 14:52:15 -0500

    Added test cases for hard-coded response

--- test/unit/test_translator.py
+++ test/unit/test_translator.py
@@ -6,8 +6,40 @@ def test_chinese():
     assert is_english == False
     assert translated_content == "This is a Chinese message"
 
+def test_hindi():
+    is_english, translated_content = translate_content("यह हिंदी में संदेश है")
+    assert is_english == False
+    assert translated_content == "This is a Hindi message"
+
+def test_english():
+    is_english, translated_content = translate_content("This is an English message")
+    assert is_english == True
+    assert translated_content == "This is an English message"
+
+def test_turkish():
+    is_english, translated_content = translate_content("Bu bir Türkçe mesajdır")
+    assert is_english == False
+    assert translated_content == "This is a Turkish message"
+
+def test_russian():
+    is_english, translated_content = translate_content("Это сообщение на русском")
+    assert is_english == False
+    assert translated_content == "This is a Russian message"
+
 def test_llm_normal_response():
     pass
 
-def test_llm_gibberish_response():
-    pass
+def test_llm_gibberish_response1():
+    is_english, translated_content = translate_content("🥲")
+    assert is_english == False
+    assert translated_content != "This is a English message"
+
+def test_llm_gibberish_response2():
+    is_english, translated_content = translate_content("30498230f")
+    assert is_english == False
+    assert translated_content != "This is a English message"
+
+def test_llm_gibberish_response3():
+    is_english, translated_content = translate_content("......")
+    assert is_english == False
+    assert translated_content != "This is a English message"
